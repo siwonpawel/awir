f245422cdb622b132a2a64cb3688810cacd3cde1
Paweł Siwoń <siwonpawel@gmail.com>
2024-04-21 12:32:23 +0200

    lab07 - spring security + REST

--- src/main/java/com/github/siwonpawel/awir/configuration/WebSecurityConfig.java
+++ src/main/java/com/github/siwonpawel/awir/configuration/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -32,10 +33,14 @@ public class WebSecurityConfig //implements WebSecurityCustomizer
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception
     {
-        return httpSecurity.
-                authorizeHttpRequests(requests ->
+        return httpSecurity
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/api/**").permitAll())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(requests ->
                         requests
-                                .requestMatchers("/", "/home").permitAll()
+                                .requestMatchers("/").permitAll()
                                 .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .formLogin(Customizer.withDefaults())
