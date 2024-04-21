c820dd8c4bcfac73c3322a2a0af0550858c39270
Paweł Siwoń <siwonpawel@gmail.com>
2024-04-21 11:56:57 +0200

    lab07 - spring security

---
+++ src/main/java/com/github/siwonpawel/awir/configuration/WebSecurityConfig.java
@@ -0,0 +1,46 @@
+package com.github.siwonpawel.awir.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig //implements WebSecurityCustomizer
+{
+
+    @Bean
+    public UserDetailsService userDetailsService()
+    {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception
+    {
+        return httpSecurity.
+                authorizeHttpRequests(requests ->
+                        requests
+                                .requestMatchers("/", "/home").permitAll()
+                                .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults())
+                .logout(LogoutConfigurer::permitAll)
+                .build();
+    }
+
+}
